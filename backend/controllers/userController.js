cb9b379a5038e54c4d6594843b739ef70ba66f2d
zahidhasan13 <zahidhasanofficial13@gmail.com>
2025-03-05 05:07:53 +0600

    jwt added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: 3 * 24 * 60 * 60,
+  });
+};
 
 const login = async (req, res) => {
   res.json({ message: "Login Route" });
@@ -8,7 +15,8 @@ const signup = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
