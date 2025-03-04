867a729052b38f768681b64c5ca3316005015d00
zahidhasan13 <zahidhasanofficial13@gmail.com>
2025-03-05 03:30:08 +0600

    bycrypt added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,13 @@ const login = async (req, res) => {
 };
 
 const signup = async (req, res) => {
-  res.json({ message: "Signup Route" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = {
